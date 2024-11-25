705e09af808f080135d6763d90c47a0ed45ce55d
unisexx <unisexx@gmail.com>
2024-11-25 09:14:00 +0700

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -28,16 +28,12 @@
 <script setup>
 import { useRoute } from "#app";
 
-import ThemeCard from "../components/ThemeCard.vue";
-import StickerCard from "../components/StickerCard.vue";
-import EmojiCard from "../components/EmojiCard.vue";
-
 const route = useRoute();
 
 const { data, error, pending } = await useAsyncData(
-  `fetchStickerUpdate`,
+  `fetchSticker`,
   async () => {
-    const res = await fetch(`${config.public.apiBase}/sticker-update`);
+    const res = await fetch(`https://line2me.in.th/sticker-update`);
     if (!res.ok) throw new Error("Failed to fetch API");
     return res.json();
   }
@@ -46,44 +42,6 @@ const { data, error, pending } = await useAsyncData(
 if (error.value) {
   console.error("Error fetching data:", error.value);
 }
-
-// const { data: themes, error: themeError } = useFetch(
-//   `${config.public.apiBase}/theme-update`
-// );
-// const { data: emojis, error: emojiError } = useFetch(
-//   `${config.public.apiBase}/emoji-update`
-// );
-
-// ตั้งค่า SEO สำหรับหน้า Index
-// useHead({
-//   title: "Line2Me - สติกเกอร์ ธีม และอิโมจิใหม่ล่าสุด",
-//   meta: [
-//     {
-//       name: "description",
-//       content:
-//         "ค้นหาสติกเกอร์ไลน์ ธีม และอิโมจิใหม่ล่าสุดจาก Line2Me พร้อมข้อมูลอัปเดตใหม่ทุกวัน รวมสินค้าลิขสิทธิ์แท้มากมายในราคาสุดคุ้ม",
-//     },
-//     {
-//       name: "keywords",
-//       content:
-//         "สติกเกอร์ไลน์, ธีมไลน์, อิโมจิไลน์, สติกเกอร์ใหม่, ธีมใหม่, อิโมจิใหม่, Line2Me, สติกเกอร์ลิขสิทธิ์, ธีมลิขสิทธิ์, อิโมจิลิขสิทธิ์",
-//     },
-//     {
-//       property: "og:title",
-//       content: "Line2Me - สติกเกอร์ ธีม และอิโมจิใหม่ล่าสุด",
-//     },
-//     {
-//       property: "og:description",
-//       content:
-//         "ค้นหาสติกเกอร์ไลน์ ธีม และอิโมจิใหม่ล่าสุดจาก Line2Me พร้อมข้อมูลอัปเดตใหม่ทุกวัน รวมสินค้าลิขสิทธิ์แท้มากมายในราคาสุดคุ้ม",
-//     },
-//     {
-//       property: "og:image",
-//       content: "https://example.com/default-og-image.jpg",
-//     },
-//     { property: "og:url", content: "https://example.com/" },
-//   ],
-// });
 </script>
 
 <style scoped>
