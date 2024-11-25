d4773bd12117dc3f6247cb57d547619b1fb46b8a
unisexx <unisexx@gmail.com>
2024-11-25 11:51:51 +0700

    ีupdated

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <h1>ข้อมูลสติกเกอร์</h1>
-    <div v-if="data">
-      <pre>{{ data }}</pre>
+    <div v-if="stickerData">
+      <!-- <pre>{{ stickerData }}</pre> -->
+      <h2 class="text-xl font-semibold mb-4">
+        สติกเกอร์ไลน์อัพเดทประจำสัปดาห์
+      </h2>
+      <StickerCard :stickers="stickerData" />
+
+      <hr />
     </div>
     <p v-else-if="pending">Loading...</p>
     <p v-else>Error loading data</p>
@@ -15,14 +21,15 @@ import { useRoute } from "#app";
 const route = useRoute();
 const id = route.params.id;
 
-const { data, error, pending } = await useAsyncData(
-  `fetchSticker-${id}`,
-  async () => {
-    const res = await fetch(`https://line2me.in.th/api/sticker-update`);
-    if (!res.ok) throw new Error("Failed to fetch API");
-    return res.json();
-  }
-);
+const {
+  data: stickerData,
+  error,
+  pending,
+} = await useAsyncData(`fetchSticker-${id}`, async () => {
+  const res = await fetch(`https://line2me.in.th/api/sticker-update`);
+  if (!res.ok) throw new Error("Failed to fetch API");
+  return res.json();
+});
 
 if (error.value) {
   console.error("Error fetching data:", error.value);
