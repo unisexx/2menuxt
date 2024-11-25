6fa7d024bb896cbba4d0f4222c9f8ad970cb19c3
unisexx <unisexx@gmail.com>
2024-11-26 01:58:32 +0700

    updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
-export default {
+module.exports = {
     apps: [
         {
             name: "line2me",
             script: "./.output/server/index.mjs",
             exec_mode: "cluster",
-            instances: "max",
+            instances: "max", // หรือกำหนดจำนวน instance เช่น 2, 3
             env: {
                 NODE_ENV: "production",
                 PORT: 3000,
