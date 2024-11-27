94bdc462ee6abcc958c758518d2d1476f03968ef
unisexx <unisexx@gmail.com>
2024-11-27 12:11:30 +0700

    updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col min-h-screen">
         <!-- Gradient Blob -->
         <div
-            class="absolute top-[-280px] right-[-100px] -z-10 w-96 h-96 bg-gradient-to-tr from-blue-400 via-purple-400 to-pink-400 opacity-50 rounded-full"
+            class="absolute top-[-280px] right-0 -z-10 w-96 h-96 bg-gradient-to-tr from-blue-400 via-purple-400 to-pink-400 opacity-50 rounded-full"
             style="filter: blur(70px)"
         ></div>
 
