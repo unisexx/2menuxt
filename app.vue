e03f46690cfcc2f477b23fa2e1a86044101a3d7a
unisexx <unisexx@gmail.com>
2024-12-05 16:45:50 +0700

    updated

--- app.vue
+++ app.vue
@@ -3,3 +3,20 @@
         <NuxtLayout> </NuxtLayout>
     </div>
 </template>
+
+<script setup>
+    useHead({
+        script: [
+            {
+                src: "https://www.googletagmanager.com/gtag/js?id=G-L2WFRL1B38",
+                async: true,
+            },
+        ],
+        innerHTML: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-L2WFRL1B38');
+  `,
+    });
+</script>
