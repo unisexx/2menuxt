54ad1f9238a20349cb834ee26d1f31c0ce5d8b61
unisexx <unisexx@gmail.com>
2024-12-10 15:05:47 +0700

    ีupdated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,8 @@
 <template>
+    <div
+        class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"
+    ></div>
+
     <div class="flex flex-col min-h-screen">
         <!-- Navbar -->
         <nav class="bg-white border-b fixed top-0 left-0 w-full z-50 shadow-sm">
